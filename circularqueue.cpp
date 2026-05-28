a9ab33148c4af66156e4a35fd94332752172b619
Rakha-arch <miftahuzahran@gmail.com>
2026-05-28 15:14:44 +0700

    mengimplementasikan logika tampilan

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -77,5 +77,30 @@ public:
             return;
         }
         cout << "\nElements in the queue are....\n";
+
+        if (FRONT_Position <= REAR_Position)
+        {
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
+        else
+        {
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+            FRONT_Position = 0;
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
     }
 };
