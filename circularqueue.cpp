dca2fc0cd26a5606af89ac30245ae0273c7f678a
Rakha-arch <miftahuzahran@gmail.com>
2026-05-28 15:10:14 +0700

    menangani kasus elemen tunggal di remove

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -52,4 +52,10 @@ public:
         }
         cout << "\nThe element deleted from the queue is: " 
         << queue_array[FRONT] << "\n";
+
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
 };
